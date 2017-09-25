86a02ef2c0b1606d6996c92534965281fdb17573
Jeroen Brouns <jeroenbrouns@wlan-197203.nbw.tue.nl>
2017-09-25 18:45:19 +0200

    Removed unnecessary code in authentication

--- Component_1/authentication.py
+++ Component_1/authentication.py
@@ -10,14 +10,3 @@ def get_db():
 
     return db
 
-def add_country(db):
-    db.countries.insert({"name" : "Canada"})
-
-def get_country(db):
-    return db.countries.find_one()
-
-if __name__ == "__main__":
-
-    db = get_db()
-    add_country(db)
-    print get_country(db)
