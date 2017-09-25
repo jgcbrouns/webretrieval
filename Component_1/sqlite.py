f043312bbbba5045be4dc03542f755e472d3e896
Tomas Sostak <tomassostak@wlan-200189.nbw.tue.nl>
2017-09-25 19:06:28 +0200

    SQLite data querying

---
+++ Component_1/sqlite.py
@@ -0,0 +1,8 @@
+import sqlite3
+conn = sqlite3.connect('data/database.sqlite')
+c = conn.cursor()
+query = 'SELECT a.name author, COUNT(DISTINCT p.Id) num_papers FROM authors a INNER JOIN paper_authors pa ON a.id=pa.author_id INNER JOIN papers p ON pa.paper_id=p.id WHERE year=2016 GROUP BY a.name ORDER BY COUNT(DISTINCT p.Id) DESC LIMIT 100'
+query = 'SELECT * FROM papers ORDER BY id DESC LIMIT 1'
+query = c.execute(query)
+for row in query:
+    print(row[5])
