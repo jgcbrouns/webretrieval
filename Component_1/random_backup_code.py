163f545444573aa64a7368d211ecb56edbe8b4e1
Tomas Sostak <tomassostak@wlan-244098.nbw.tue.nl>
2017-10-16 20:50:00 +0200

    Fixing commit

--- Component_1/random_backup_code.py
+++ Component_1/random_backup_code.py
@@ -65,3 +65,80 @@
 
 # def say_hi(db):
 #     return "test"
+
+
+       # print titleFiltered
+        # print type('Analysis of a greedy active learning strategy')
+        # print unicode(str(title[0]))
+        # wordListForTitle = re.sub("[^\w]", " ",  title[0]).split()
+        # for word in wordListForTitle:
+        #print title[0]
+        #print "\n"
+# 'Analysis of a greedy active learning strategy'
+
+
+    # for title in titles:
+    #     #print "Looking for title: " + str(title[0])
+    #     wordListForTitle = re.sub("[^\w]", " ",  title[0]).split()
+    #     # print "but actually looking for words:"
+    #     # for titleWord in wordListForTitle:
+    #     #     print titleWord
+    #     for text in texts:
+    #     # for part in text:
+    #         # print "******************start**********************"
+    #         # print text[6]
+    #         # print "******************end**********************"
+            
+          
+    #         if title[0] in text[6]:
+    #             print "I found the title: " + str(title[0]) + " in: "
+    #             print text[6]
+
+            # filter = "References"
+            # if filter in str(text):
+            #     referencesText = ((str(text)).split(filter,1)[1])
+           
+            #     wordListForReferences = re.sub("[^\w]", " ",  referencesText).split()
+                
+            #     counter = 0
+            #     for word in wordListForReferences:
+            #         if word == wordListForTitle[counter]:
+
+            #             counter = counter + 1
+            #             if counter == 3:                              
+            #                 print "I found the title: " + str(title[0]) + " in: "
+            #                 print referencesText
+            #                 counter = 0
+
+
+
+
+       # for text in texts:
+       #      tokens = Data.prepare(text[6])
+       #      counter = 0
+       #      for token in tokens:
+       #          if token == wordListForTitle[counter]:
+       #              counter = counter + 1
+       #              if counter == 3:                              
+       #                  print "I found the title: " + str(title[0]) + " in: "
+       #                  print token
+       #                  counter = 0
+
+
+                   # filter = "References"
+            # if filter in str(document[6]):
+            #     referencesText = ((str(document[6])).split(filter,1)[1])
+           
+            #     wordListForReferences = re.sub("[^\w]", " ",  referencesText).split()
+                
+            #     counter = 0
+            #     for word in wordListForReferences:
+            #         if word == wordListForTitle[counter]:
+
+            #             counter = counter + 1
+            #             if counter == 5 || counter == len(wordListForTitle):                              
+            #                 print "I found the title: " + str(title[0]) + " in: "
+            #                 print referencesText
+            #                 counter = 0
+            #         else:
+            #             counter = 0
--- Component_1/repository.py
+++ Component_1/repository.py
@@ -27,3 +27,40 @@ def update_amount(db, keyword, documentId, amount):
     add_amount(db, keyword, documentId, amount)
 
 
+'''
+    adds an amount to the database for a specific keyword and documentId
+    @param db, keyword, documentId, amount
+'''
+def add_reference_for_title(db, title, documentIdList, titleId):
+    db.title_references2.update(
+        { 'title': title, 'documentId': titleId },
+        { '$push': {'references_found': {'documents': str(documentIdList)}}}, upsert=True, multi=True
+    )
+
+
+'''
+    Gets all the references in the colleciton title_references
+    @param db
+'''
+def get_references(db):
+    cursor = db.title_references2.find({})
+    return cursor
+
+
+'''
+    Returns title for a specific documentId
+    @param db, documentId
+'''
+def get_title_for_document_id(db, documentId):
+    cursor = db.title_references2.find_one({'documentId': documentId })
+    return cursor
+
+
+'''
+    adds a pagerank value for a specific documentId
+    @param db, pageRank, documentId
+'''
+def add_pagerank(db, documentId, pageRank):
+    db.pagerank.insert(
+        { 'documentId': documentId, 'PageRank': pageRank,  }
+    )
